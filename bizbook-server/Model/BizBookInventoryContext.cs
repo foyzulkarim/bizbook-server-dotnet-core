836b5f912a79b76aefb24ccebf40f7e3b6d49393
Foyzul Karim <Foyzul.Karim@Nintex.com>
2019-05-18 13:25:57 +0800

    database created

--- bizbook-server/Model/BizBookInventoryContext.cs
+++ bizbook-server/Model/BizBookInventoryContext.cs
@@ -12,16 +12,52 @@ namespace Model
 {
     public class BizBookInventoryContext : DbContext
     {
+        #region Products
         public virtual DbSet<Brand> Brands { get; set; }
-        public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<Address> Addresses { get; set; }
-        public virtual DbSet<Dealer> Dealers { get; set; }
-        public virtual DbSet<District> Districts { get; set; }
+
         public virtual DbSet<ProductCategory> ProductCategories { get; set; }
+
         public virtual DbSet<ProductGroup> ProductGroups { get; set; }
-        public virtual DbSet<Shop> Shops { get; set; }
+
+        public virtual DbSet<ProductDetail> ProductDetails { get; set; }
+
+        #endregion
+
+        #region Sales
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        public virtual DbSet<Address> Addresses { get; set; }
+
+        public virtual DbSet<Dealer> Dealers { get; set; }
+
+        public virtual DbSet<Sale> Sales { get; set; }
+        public virtual DbSet<SaleDetail> SaleDetails { get; set; }
+        public virtual DbSet<SaleState> SaleStates { get; set; }
+        #endregion
+
+        #region Purchases
         public virtual DbSet<Supplier> Suppliers { get; set; }
+
+        public virtual DbSet<Purchase> Purchases { get; set; }
+
+        public virtual DbSet<PurchaseDetail> PurchaseDetails { get; set; }
+
+        #endregion
+
+        #region Transactions
+
         public virtual DbSet<Transaction> Transactions { get; set; }
+
+        public virtual DbSet<AccountHead> AccountHeads { get; set; }
+        public virtual DbSet<Wallet> Wallets { get; set; }
+
+        #endregion
+
+
+        public virtual DbSet<District> Districts { get; set; }
+
+        public virtual DbSet<Shop> Shops { get; set; }
+
         public virtual DbSet<Employee> Employees { get; set; }
 
         public BizBookInventoryContext(DbContextOptions<BizBookInventoryContext> options) : base(options)
