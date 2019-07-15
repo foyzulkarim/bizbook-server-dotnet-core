2536bbd36645c5c93b761ec01d87b8fb74eecced
Foyzul Karim <Foyzul.Karim@Nintex.com>
2019-07-15 09:14:16 +0800

    product group service added

---
+++ bizbook-server/ApiTestProject/ProductGroupQueryControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Model.Products;
+using RequestModel.Shops;
+using RestSharp;
+using Shouldly;
+
+namespace ApiIntegrationTestProject
+{
+    [TestClass]
+    public class ProductGroupQueryControllerTest
+    {
+        [TestMethod]
+        public void ProductGroupQueryTest()
+        {
+            var client = new RestClient(Constants.BaseUrl);
+            var request = new RestRequest("api/ProductGroupQuery/Search", Method.POST, DataFormat.Json);
+            var token = TestHelper.GetToken();
+            request.AddHeader("Authorization", $"Bearer {token}");
+
+            var model = new BrandRequestModel("")
+            {
+                ShopId = "1"
+            };
+
+            model.ShopId.ShouldNotBeNullOrWhiteSpace();
+            request.AddJsonBody(model);
+
+            var response = client.Execute(request);
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+    }
+}
