b5387997b76a8fab297c13ac10092baf09fa4b3a
torcop <animesh.ruet@gmail.com>
2019-07-20 16:40:07 +0600

    Feature/productgroup crud (#4)
    * product group service added
    
    * ordered sequence modelbinder provider
    
    * product group test add/ query done

--- bizbook-server/ApiTestProject/ProductGroupQueryControllerTest.cs
+++ bizbook-server/ApiTestProject/ProductGroupQueryControllerTest.cs
@@ -2,40 +2,34 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
-using B2BCoreApi.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Model.Model;
 using Model.Model.Products;
+using RequestModel.Products;
+using RequestModel.Shops;
 using RestSharp;
 using Shouldly;
 
 namespace ApiIntegrationTestProject
 {
     [TestClass]
-    public class ProductGroupControllerTest
+    public class ProductGroupQueryControllerTest
     {
         [TestMethod]
-        public void ProductGroupAddTest()
+        public void ProductGroupQueryTest()
         {
             var client = new RestClient(Constants.BaseUrl);
-            var request = new RestRequest("api/ProductGroup/Add", Method.POST, DataFormat.Json);
+            var request = new RestRequest("api/ProductGroupQuery/Search", Method.POST, DataFormat.Json);
             var token = TestHelper.GetToken();
             request.AddHeader("Authorization", $"Bearer {token}");
 
-            ProductGroup model = new ProductGroup()
+            var model = new ProductGroupRequestModel("")
             {
-                Id = Guid.NewGuid().ToString(),
-                Created = DateTime.Now,
-                Modified = DateTime.Now,
-                CreatedBy = "1",
-                ModifiedBy = "1",
-                CreatedFrom = "Test",
-                IsActive = true,
-                ShopId = "1",
-                Name = $"ProductGroup-{DateTime.Now:ssmmhhddMMyyyy}",
+                ShopId = "1"
             };
 
+            model.ShopId.ShouldNotBeNullOrWhiteSpace();
             request.AddJsonBody(model);
+
             var response = client.Execute(request);
             response.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
